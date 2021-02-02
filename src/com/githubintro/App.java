cd689511f12696e8e3d1641b5b51a4d291852586
Tapkovsky <mr.lev4ik2001@mail.ru>
2021-02-02 13:15:23 +0300

    Изменил текст с Hello world на Goodbye

--- src/com/githubintro/App.java
+++ src/com/githubintro/App.java
@@ -2,6 +2,6 @@ package com.githubintro;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Goodbye");
     }
 }
